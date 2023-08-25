347639abb5a9d88982665769ed525dc76f5fbddc
Eduardo Quartiero <eduardoqm27@gmail.com>
2023-08-25 09:01:32 -0300

    nodejs-EQM27

--- demo_http.js
+++ demo_http.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
-//create a server object:
+//criar um objeto de servidor:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080 
+  res.write('Hello World!'); //escreva uma resposta ao cliente
+  res.end(); //terminar a resposta
+}).listen(8081); //o objeto servidor escuta na porta 8080 
